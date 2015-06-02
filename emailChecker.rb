4ecf1ae5397ab4d65f26cb8b583f0582d6741791
David Cunningham <david@preferral.com>
2015-06-02 12:20:37 -0500

    corrected license

--- emailChecker.rb
+++ emailChecker.rb
@@ -9,19 +9,19 @@ end
 
 #Non json field based import
 get '/validate' do
-	erb :emailverificationform
+  erb :emailverificationform
 end
 
 
 #json based field
 post '/api/validate.json' do
-	data = Hash.new
-	begin
-		data['email'] = params[:email]
-		data['validity'] = EmailVerifier.check(data['email'])
-		data['validFormat'] = true
-	rescue
-		data['validFormat'] = false
-	end
-	data.to_json
+  data = Hash.new
+  begin
+    data['email'] = params[:email]
+    data['validity'] = EmailVerifier.check(data['email'])
+    data['validFormat'] = true
+  rescue
+    data['validFormat'] = false
+  end
+  data.to_json
 end
--- public/script.js
+++ public/script.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
-	$('#button').click(function(){
-		var myEmail = $('input[name=checkListItem]').val();
-		$.ajax({
-			dataType: 'json',
-			url: '/api/validate.json',
-			type: "POST",
-			data: {email: myEmail},
-			success: function(data){
-				debugger;
-				if (data.validFormat == false){
-					window.alert("Email Address: " + data.email+" is not in a valid format.\n\n\nPlease submit address in following format:\n\nrealname@realdomain.com");
-				}else{
-					if (data.validity == true) {
-						$('.list').append('<div class="item">'+data.email+' is valid</div>');
-						$('.item:last-child').css('color', '#00ff00')
-					} else{
-						$('.list').append('<div class="item">'+data.email+' is not valid</div>');
-						$('.item:last-child').css('color', '#cc0000')
-					};
-				};
-			},
-			error: function(jqXHR, textStatus, errorThrown){
-				debugger
-				window.alert('Error '+ jqXHR.status +': ' + jqXHR.statusText)
-			}
-		});
-	});
+  $('#button').click(function(){
+    var myEmail = $('input[name=checkListItem]').val();
+    $.ajax({
+      dataType: 'json',
+      url: '/api/validate.json',
+      type: "POST",
+      data: {email: myEmail},
+      success: function(data){
+        debugger;
+        if (data.validFormat == false){
+          window.alert("Email Address: " + data.email+" is not in a valid format.\n\n\nPlease submit address in following format:\n\nrealname@realdomain.com");
+        }else{
+          if (data.validity == true) {
+            $('.list').append('<div class="item">'+data.email+' is valid</div>');
+            $('.item:last-child').css('color', '#00ff00')
+          } else{
+            $('.list').append('<div class="item">'+data.email+' is not valid</div>');
+            $('.item:last-child').css('color', '#cc0000')
+          };
+        };
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+        debugger
+        window.alert('Error '+ jqXHR.status +': ' + jqXHR.statusText)
+      }
+    });
+  });
 });
