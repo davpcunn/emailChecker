5e926b90d19e962cd1e5cc6a94147c013c2c6b9a
David Cunningham <david@preferral.com>
2015-06-17 11:09:40 -0500

    added google search, integrated email_verifier

--- email_guess.rb
+++ email_guess.rb
@@ -4,22 +4,40 @@ require 'faroo'
 require 'open-uri'
 require 'dotenv'
 require 'net/http'
+require 'twitter'
+require 'google_search'
+
+def validate(email) do
+  begin
+    valiid
+    data['validity'] = EmailVerifier.check(data['email'])
+    data['error'] = false
+  rescue EmailVerifier::OutOfMailServersException
+    data['error'] = true
+    data['errorText'] = "out_of_mail_server: appears to point to dead mail server"
+  rescue EmailVerifier::NoMailServerException
+    data['error'] = true
+    data['errorText'] = "no_mail_server: appears to point to domain which doesn't handle e-mail"
+  rescue EmailVerifier::FailureException
+    data['error'] = true
+    data['errorText'] = "failure: could not be checked if is real"
+  end
+  return data
+end
 
 def get_possible_names(employee) #takes employee name and turns it into local patterns
 end
 
-
-def check_names(locals, domain) #checks all names for a certain domain
-  uri = URI(ENV['email_verifier url'])
+def check_emails(locals, domain) #checks all names for a certain domain
   all_valid = true
   valid_addresses = []
   locals.each do |local|
-    address = local + '@' + domain
-    res = Net::HTTP.post_form(uri, 'email' => address)
-    if res.body.error #break if you experience an error
+    email = local + '@' + domain
+    data = validate(address)
+    if data['error'] #break if you experience an error
       break
-    elsif res.body.valid #if it's valid add it
-      valid_addresses.push
+    elsif data['valid'] #if it's valid add it
+      valid_addresses.push(email)
     else
       all_valid = false
   end
@@ -30,24 +48,54 @@ def check_names(locals, domain) #checks all names for a certain domain
   end
 end
 
-def faroo_search(co_name, locals)
+def faroo_search(co_name)
   addresses = []
-  #get api key
-  key = ENV['API_key']
   #get results
-  data = Faroo.new(key).web(co_name)
-  data.results.each do |item|
-    puts item.domain
-    (addresses << evaluate_addresses(locals, item.domain)).flatten!
+  Faroo.new(ENV['API_key']).web(co_name).results.each do |item|
+    addresses << item.domain
   end
   return addresses
 end
 
-def twitter_search(locals, co_name)
-  #search twitter for company name
-  #take the first 10 results
-  results.each do |profile|
-    #if there is a website on the profile, check names on the website
-  end
+def google_search(co_name)
+  serach = Google::Search::Web.new do |search|
+    search.query = co_name
+    search.size = :small
+  end.map(&:domain)
+  return search
 end
 
+def twitter_search(co_name)
+  addresses = []
+  #configure twitter client
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
+    config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
+  end
+  results = client.user_search(co_name)#search twitter for company name
+  results.each do |user| #search twitter for company's website
+    if user.website?
+      addresses << user.website
+    end
+end
+
+EmailVerifier.config do |config|
+  config.verifier_email = ENV['base_email']
+end
+
+post '/api/guess.json' do
+  data=Hash.new
+  data['emp_name'] = params[:emp_name]
+  data['co_name'] = params[:co_name]
+  data['verified_emails'] = []
+  data['checked_domains'] = []
+  locals = get_possible_names(data['emp_name'])
+  (data['checked_domains'] << faroo_search(data['co_name'])
+  (data['checked_domains'] << twitter_search(data['co_name'])
+  data['checked_domain'].each do |address|
+    data['verified_emails'] << check_emails(address, locals)
+  end
+  data.to_json
+end
