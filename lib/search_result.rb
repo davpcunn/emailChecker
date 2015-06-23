292c482d6703057bafe1145e5ea991d1d10afb7d
David Cunningham <david@preferral.com>
2015-06-23 11:34:44 -0500

    search result now takes title weight and domain

--- lib/search_result.rb
+++ lib/search_result.rb
@@ -1,12 +1,16 @@
+require 'levenshtein'
+require 'rubygems'
+require 'domainatrix'
+
 class SearchResult
   @rank = -1
-
-  attr_accessor :uti, :title, :rank, :titleWeight
+  attr_accessor :uti, :title, :rank, :titleWeight, :domain
   def initialize(result, rank, query)
     @uri = result.uri
     @title = result.title
     @rank = rank
-    @titleWeight
+    @domain  = Domainatrix.parse(@uri).domain
+    @titleWeight = distance(@title, query)
   def getTitleWeight(title)
   end
 end
